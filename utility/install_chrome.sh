98627476a91b861a57057ea20819b4331743b9f8
Mikko Nieminen <mikko.nieminen@bihealth.de>
2017-10-12 18:16:53 +0200

    fix install_chrome.sh for ci

--- utility/install_chrome.sh
+++ utility/install_chrome.sh
@@ -8,19 +8,19 @@ echo "***********************************************"
 CHROME_DRIVER_VERSION=2.33
 
 # Install dependencies
-sudo apt-get update
-sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
+apt-get update
+apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 # Install Chrome
 wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
-sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
-sudo apt-get -f install -y
-sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
+dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
+apt-get -f install -y
+dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
 
 # Install ChromeDriver
 wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
+mv -f ~/chromedriver /usr/local/bin/chromedriver
+chown root:root /usr/local/bin/chromedriver
+chmod 0755 /usr/local/bin/chromedriver
