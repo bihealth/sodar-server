ed23c8d7624e6678210044071e96c305426644b1
Mikko Nieminen <mikko.nieminen@bihealth.de>
2018-02-06 13:53:15 +0100

    add general samplesheets info displaying (#79)

--- samplesheets/templatetags/samplesheets_tags.py
+++ samplesheets/templatetags/samplesheets_tags.py
@@ -1,5 +1,16 @@
 from django import template
 
+from ..models import Investigation
+
 
 register = template.Library()
 
+
+@register.simple_tag
+def get_investigation(project):
+    """Return Investigation for a project"""
+    try:
+        return Investigation.objects.get(project=project)
+
+    except Investigation.DoesNotExist:
+        return None
