4b0299d79c66fac54f27cfbe026d5dd943ef1439
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-06-28 11:35:45 +0200

    Allowing arbitrary file extensions to links exposed to IGV.

--- samplesheets/studyapps/germline/urls.py
+++ samplesheets/studyapps/germline/urls.py
@@ -7,12 +7,12 @@ app_name = 'samplesheets.studyapps.germline'
 
 urlpatterns = [
     url(
-        regex=r'^file/(?P<file_type>[a-z]+)/(?P<genericmaterial>[0-9a-f-]+)$',
+        regex=r'^file/(?P<file_type>[a-z]+)/(?P<genericmaterial>[0-9a-f-]+)(\..*)?$',
         view=views.FileRedirectView.as_view(),
         name='file',
     ),
     url(
-        regex=r'^render/igv/(?P<genericmaterial>[0-9a-f-]+)$',
+        regex=r'^render/igv/(?P<genericmaterial>[0-9a-f-]+)(\..*)?$',
         view=views.IGVSessionFileRenderView.as_view(),
         name='igv',
     ),
