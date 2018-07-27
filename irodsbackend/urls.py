0dc52180c95fecfe76c047543e6d2593826b033b
Mikko Nieminen <mikko.nieminen@bihealth.de>
2018-07-27 15:37:07 +0200

    fix irodsbackend url patterns (#304)

--- irodsbackend/urls.py
+++ irodsbackend/urls.py
@@ -7,12 +7,12 @@ app_name = 'irodsbackend'
 
 urlpatterns = [
     url(
-        regex=r'^stats/(?P<project>[0-9a-f-]+)\?path=(?P<path>[\w\-_/]+)$',
+        regex=r'^stats/(?P<project>[0-9a-f-]+)\?path=(?P<path>[\w\-_/@]+)$',
         view=views.IrodsStatisticsGetAPIView.as_view(),
         name='stats',
     ),
     url(
-        regex=r'^stats\?path=(?P<path>[\w\-_/]+)$',
+        regex=r'^stats\?path=(?P<path>[\w\-_/@]+)$',
         view=views.IrodsStatisticsGetAPIView.as_view(),
         name='stats',
     ),
