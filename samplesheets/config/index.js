03b573b77dbca8e92c87c354a28d48b6856698e5
mikko.nieminen <mikko.nieminen@bihealth.de>
2019-05-20 12:01:07 +0200

    another path fix attempt (#512)

--- samplesheets/config/index.js
+++ samplesheets/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'samplesheets-vue',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/static/',
 
     /**
      * Source Maps
