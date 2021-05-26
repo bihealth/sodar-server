60fcac3138e4a9691b5426d849b167fda99b1417
Mikko Nieminen <mikko.nieminen@bihealth.de>
2021-05-26 11:36:18 +0200

    fix attempt for #1151

--- config/urls.py
+++ config/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     # Iconify SVG icons
     url(r'icons/', include('dj_iconify.urls')),
     # General site apps
-    url(r'^alerts/admin/', include('adminalerts.urls')),
+    url(r'^alerts/adm/', include('adminalerts.urls')),
     url(r'^alerts/app/', include('appalerts.urls')),
     url(r'^siteinfo/', include('siteinfo.urls')),
     url(r'^irods/', include('irodsinfo.urls')),
