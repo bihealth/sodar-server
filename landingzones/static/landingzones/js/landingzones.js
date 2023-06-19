9120fff40196b2c500320feed14ddafc4fc9279a
gromdimon <grom.dima.grom@gmail.com>
2023-06-19 16:16:12 +0200

    POST implementation

--- landingzones/static/landingzones/js/landingzones.js
+++ landingzones/static/landingzones/js/landingzones.js
@@ -2,6 +2,8 @@
  Zone status updating function
  *****************************/
 var updateZoneStatus = function() {
+    var zoneUuids = [];
+
     $('.sodar-lz-zone-tr-existing').each(function() {
         var trId = $(this).attr('id');
         var zoneTr = $('#' + trId);
@@ -10,68 +12,88 @@ var updateZoneStatus = function() {
         var statusTd = zoneTr.find('td#sodar-lz-zone-status-' + zoneUuid);
 
         if (statusTd.text() !== 'MOVED' && statusTd.text() !== 'DELETED') {
-            $.ajax({
-                url: $(this).attr('data-status-url'),
-                method: 'GET',
-                dataType: 'json'
-            }).done(function (data) {
-                // console.log(trId + ': ' + data['status']);  // DEBUG
-                var statusInfoSpan = zoneTr.find('span#sodar-lz-zone-status-info-' + zoneUuid);
-                // TODO: Should somehow get these from STATUS_STYLES instead
-                var statusStyles = {
-                    'CREATING': 'bg-warning',
-                    'NOT CREATED': 'bg-danger',
-                    'ACTIVE': 'bg-info',
-                    'PREPARING': 'bg-warning',
-                    'VALIDATING': 'bg-warning',
-                    'MOVING': 'bg-warning',
-                    'MOVED': 'bg-success',
-                    'FAILED': 'bg-danger',
-                    'DELETING': 'bg-warning',
-                    'DELETED': 'bg-secondary'
-                };
+            zoneUuids.push(zoneUuid);
+        }
+    });
 
-                if (statusTd.text() !== data['status'] ||
-                        statusInfoSpan.text() !== data['status_info']) {
-                    statusTd.text(data['status']);
+    // Make the POST request to retrieve zone statuses
+    $.ajax({
+        url: zoneStatusUrl,
+        method: 'POST',
+        dataType: 'json',
+        data: {
+            zone_uuids: zoneUuids
+        }
+    }).done(function(data) {
+        $('.sodar-lz-zone-tr-existing').each(function() {
+            var zoneUuid = $(this).attr('data-zone-uuid');
+            var zoneTr = $('#' + $(this).attr('id'));
+            var statusTd = zoneTr.find('td#sodar-lz-zone-status-' + zoneUuid);
+            var statusInfoSpan = zoneTr.find('span#sodar-lz-zone-status-info-' + zoneUuid);
+            var statusStyles = {
+                'CREATING': 'bg-warning',
+                'NOT CREATED': 'bg-danger',
+                'ACTIVE': 'bg-info',
+                'PREPARING': 'bg-warning',
+                'VALIDATING': 'bg-warning',
+                'MOVING': 'bg-warning',
+                'MOVED': 'bg-success',
+                'FAILED': 'bg-danger',
+                'DELETING': 'bg-warning',
+                'DELETED': 'bg-secondary'
+            };
+
+            if (data[zoneUuid]) {
+                var zoneStatus = data[zoneUuid].status;
+                var zoneStatusInfo = data[zoneUuid].status_info;
+
+                if (
+                    statusTd.text() !== zoneStatus ||
+                    statusInfoSpan.text() !== zoneStatusInfo
+                ) {
+                    statusTd.text(zoneStatus);
                     statusTd.removeClass();
-                    statusTd.addClass(statusStyles[data['status']] + ' text-white');
-                    statusInfoSpan.text(data['status_info']);
-                    if (['PREPARING', 'VALIDATING', 'MOVING'].includes(data['status'])) {
+                    statusTd.addClass(statusStyles[zoneStatus] + ' text-white');
+                    statusInfoSpan.text(zoneStatusInfo);
+
+                    if (['PREPARING', 'VALIDATING', 'MOVING'].includes(zoneStatus)) {
                         statusTd.append(
-                            '<span class="pull-right"><i class="iconify" data-icon="mdi:lock"></i></span>')
+                            '<span class="pull-right"><i class="iconify" data-icon="mdi:lock"></i></span>'
+                        );
                     }
 
-                    if (['CREATING', 'NOT CREATED', 'MOVED', 'DELETED'].includes(data['status'])) {
+                    if (['CREATING', 'NOT CREATED', 'MOVED', 'DELETED'].includes(zoneStatus)) {
                         zoneTr.find('p#sodar-lz-zone-stats-container-' + zoneUuid).hide();
 
-                        if (data['status'] === 'MOVED') {
+                        if (zoneStatus === 'MOVED') {
                             var statusMovedSpan = zoneTr.find(
-                                'span#sodar-lz-zone-status-moved-' + zoneUuid);
+                                'span#sodar-lz-zone-status-moved-' + zoneUuid
+                            );
                             statusMovedSpan.html(
-                                '<p class="mb-0"><a href="' + sampleUrl + '">' +
+                                '<p class="mb-0"><a href="' +
+                                sampleUrl +
+                                '">' +
                                 '<i class="iconify" data-icon="mdi:arrow-right-circle"></i> ' +
-                                'Browse files in sample sheet</a></p>');
+                                'Browse files in sample sheet</a></p>'
+                            );
                         }
                     }
 
                     // Button modification
-                    if (data['status'] !== 'ACTIVE' && data['status'] !== 'FAILED') {
+                    if (zoneStatus !== 'ACTIVE' && zoneStatus !== 'FAILED') {
                         zoneTr.find('td.sodar-lz-zone-title').addClass('text-muted');
                         zoneTr.find('td.sodar-lz-zone-assay').addClass('text-muted');
                         zoneTr.find('td.sodar-lz-zone-status-info').addClass('text-muted');
                         zoneTr.find('.btn').each(function() {
-                           if ($(this).is('button')) {
-                               $(this).attr('disabled', 'disabled');
-                           }
-                           else if ($(this).is('a')) {
-                               $(this).addClass('disabled');
-                           }
-                           $(this).tooltip('disable');
+                            if ($(this).is('button')) {
+                                $(this).attr('disabled', 'disabled');
+                            } else if ($(this).is('a')) {
+                                $(this).addClass('disabled');
+                            }
+                            $(this).tooltip('disable');
                         });
                         zoneTr.find('.sodar-list-dropdown').addClass('disabled');
-                    }
-                    else {
+                    } else {
                         zoneTr.find('td.sodar-lz-zone-title').removeClass('text-muted');
                         zoneTr.find('td.sodar-lz-zone-assay').removeClass('text-muted');
                         zoneTr.find('td.sodar-lz-zone-status-info').removeClass('text-muted');
@@ -86,8 +108,8 @@ var updateZoneStatus = function() {
                         zoneTr.find('.sodar-list-dropdown').removeClass('disabled');
                     }
                 }
-            });
-        }
+            }
+        });
     });
 };
 
@@ -99,7 +121,7 @@ $(document).ready(function() {
     var statusInterval = window.statusInterval;
 
     // Poll and update active zones
-    setInterval(function () {
+    setInterval(function() {
         updateZoneStatus();
     }, statusInterval);
 
--- landingzones/views_ajax.py
+++ landingzones/views_ajax.py
@@ -11,19 +11,31 @@ from landingzones.models import LandingZone
 class ZoneStatusRetrieveAjaxView(SODARBaseProjectAjaxView):
     """Ajax API view for returning the landing zone status"""
 
-    def get_permission_required(self):
-        zone = LandingZone.objects.filter(
-            sodar_uuid=self.kwargs['landingzone']
-        ).first()
-        return (
-            'landingzones.view_zone_own'
-            if zone.user == self.request.user
-            else 'landingzones.view_zone_all'
+    permission_required = 'landingzones.view_zone_own'
+
+    def check_zone_permission(self, zone, user):
+        permission = 'landingzones.view_zone_own' if zone.user == self.request.user else 'landingzones.view_zone_all'
+        return user.has_perm(permission, obj=zone)
+
+    def post(self, request, *args, **kwargs):
+        zone_uuids = request.data.get('zone_uuids', [])
+        project = self.get_project()
+
+        # Filter landing zones based on UUIDs and project
+        zones = LandingZone.objects.filter(
+            sodar_uuid__in=zone_uuids, project=project
         )
 
-    def get(self, *args, **kwargs):
-        zone = LandingZone.objects.filter(
-            sodar_uuid=self.kwargs['landingzone']
-        ).first()
-        ret_data = {'status': zone.status, 'status_info': zone.status_info}
-        return Response(ret_data, status=200)
+        status_dict = {}
+        for zone in zones:
+            # Check permissions
+            if not self.check_zone_permission(zone, self.request.user):
+                continue
+
+            status_dict[zone.sodar_uuid] = {
+                'status': zone.status,
+                'status_info': zone.status_info
+            }
+
+        return Response(status_dict, status=200)
+
