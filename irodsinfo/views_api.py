7e84d86b05e779117216bc28bfe2312764da7c7e
gromdimon <grom.dima.grom@gmail.com>
2023-06-19 13:39:09 +0200

    implementation of views/urls/mixins/etc.

---
+++ irodsinfo/views_api.py
@@ -0,0 +1,49 @@
+"""REST API views for the irodsinfo app"""
+
+import logging
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# Projectroles dependency
+from projectroles.plugins import get_backend_api
+
+from irodsinfo.views import IrodsConfigMixin
+
+
+logger = logging.getLogger(__name__)
+
+
+class IrodsConfigRetrieveAPIView(IrodsConfigMixin, APIView):
+
+    """
+    Retrieve iRODS configuration file for the current user.
+
+    **URL:** ``/samplesheets/api/investigation/retrieve/{Project.sodar_uuid}``
+
+    **Methods:** ``GET``
+
+    **Returns:**
+
+    - ``irods_environment``: Return user iRODS config (``json``)
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        """Get iRODS config"""
+        try:
+            irods_backend = get_backend_api('omics_irods')
+            if not irods_backend:
+                return Response(
+                    {'detail': 'iRODS backend not enabled'}, status=404
+                )
+            config = self.create_env_config(request.user, irods_backend)
+            return Response({'irods_environment': config})
+
+        except Exception as ex:
+            logger.error('iRODS config retrieval failed: {}'.format(ex))
+            return Response(
+                {'detail': 'iRODS config retrieval failed'}, status=400
+            )
