90641d79dc87a5985b686f2538147489ce3369c6
Mikko Nieminen <mikko.nieminen@bih-charite.de>
2026-09-01 14:13:27 +0200

    update getFieldHeader() tests (#2497)

--- samplesheets/vue3app/tests/unit/gridUtilsGetFieldHeader.spec.ts
+++ samplesheets/vue3app/tests/unit/gridUtilsGetFieldHeader.spec.ts
@@ -1,43 +1,68 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import {
   type CellClassFunc,
   type CellClassParams,
-  type ColDef,
   type Column,
 } from 'ag-grid-community'
 
 import { getFieldHeader } from '@/utils/gridUtils.ts'
 import {
+  type FieldHeaderGetParams,
   type SheetTableFieldHeader,
   type SodarContextLinkLabel,
 } from '@/types.ts'
-import { EDIT_COL_TYPE_NAME, EDIT_COL_TYPE_ONTOLOGY } from '@/constants.ts'
+import {
+  EDIT_COL_TYPE_NAME,
+  EDIT_COL_TYPE_NUMERIC,
+  EDIT_COL_TYPE_ONTOLOGY,
+  EDIT_COL_TYPE_UNIT
+} from '@/constants.ts'
 
+import { copy } from '../testUtils.ts'
 import { sodarContext } from '../data/sodarContext.ts'
+
+// Test Data -------------------------------------------------------------------
+
+const fieldHeader = {
+  value: 'Organism',
+  col_type: EDIT_COL_TYPE_ONTOLOGY
+} as SheetTableFieldHeader
+const linkLabels = sodarContext.external_link_labels as {
+  [key: string]: string | SodarContextLinkLabel }
+const defaultParams: FieldHeaderGetParams = {
+  colAlign: 'left',
+  colWidth: 250,
+  editMode: false,
+  externalLinkLabels: linkLabels,
+  fieldEditable: true,
+  fieldHeader: fieldHeader,
+  fieldIdx: 2,
+  fieldVisible: true,
+  minColWidth: 50,
+}
+let params: FieldHeaderGetParams
 
 // Tests -----------------------------------------------------------------------
 
 describe('getFieldHeader()', () => {
-  const fieldHeader = {
-    value: 'Organism',
-    col_type: EDIT_COL_TYPE_ONTOLOGY
-  } as SheetTableFieldHeader
-  const linkLabels = sodarContext.external_link_labels as {
-    [key: string]: string | SodarContextLinkLabel }
+  function getCellClassParams (
+      colType: string,
+      editable?: boolean,
+      newInit?: boolean,
+  ): CellClassParams {
+    return {
+      colDef: { cellRendererParams: { colType: colType } },
+      column: {} as Column,
+      value: { editable: editable, newInit: newInit, value: 'x' }
+    } as CellClassParams
+  }
+
+  beforeEach(() => {
+    params = copy(defaultParams) as FieldHeaderGetParams
+  })
 
   test('get field header', async () => {
-    // TODO: Set up test data in a variable and update
-    const res: ColDef = getFieldHeader({
-      colAlign: 'left',
-      colWidth: 250,
-      editMode: false,
-      externalLinkLabels: linkLabels,
-      fieldEditable: true,
-      fieldHeader: fieldHeader,
-      fieldIdx: 2,
-      fieldVisible: true,
-      minColWidth: 50,
-    })
+    const res = getFieldHeader(params)
     expect(res.headerName).toBe('Organism')
     expect(res.field).toBe('col2')
     expect(res.width).toBe(250)
@@ -46,64 +71,74 @@ describe('getFieldHeader()', () => {
     expect(res.cellRendererParams.colType).toBe(EDIT_COL_TYPE_ONTOLOGY)
     expect(res.cellRendererParams.editMode).toBe(false)
     expect(res.cellRendererParams.fieldEditable).toBe(true)
-    expect(res.cellRendererParams.linkLabels).toBe(linkLabels)
+    expect(res.cellRendererParams.linkLabels).toEqual(linkLabels)
     expect(res.cellClass).toContain('text-left')
     expect(res.cellClass).not.toContain('text-right')
   })
 
   test('get field header with fieldVisible=false', async () => {
-    const res: ColDef = getFieldHeader({
-      colAlign: 'left',
-      colWidth: 250,
-      editMode: false,
-      externalLinkLabels: linkLabels,
-      fieldEditable: true,
-      fieldHeader: fieldHeader,
-      fieldIdx: 2, // Updated
-      fieldVisible: false,
-      minColWidth: 50,
-    })
+    params.fieldVisible = false
+    const res = getFieldHeader(params)
     expect(res.hide).toBe(true)
   })
 
   test('get field header with right aligned content', async () => {
-    const res: ColDef = getFieldHeader({
-      colAlign: 'right',  // Updated
-      colWidth: 250,
-      editMode: false,
-      externalLinkLabels: linkLabels,
-      fieldEditable: true,
-      fieldHeader: fieldHeader,
-      fieldIdx: 2,
-      fieldVisible: true,
-      minColWidth: 50,
-    })
+    params.colAlign = 'right'
+    const res = getFieldHeader(params)
     expect(res.cellClass).not.toContain('text-left')
     expect(res.cellClass).toContain('text-right')
   })
 
   test('get field header with editMode=true', async () => {
-    const res: ColDef = getFieldHeader({
-      colAlign: 'left',
-      colWidth: 250,
-      editMode: true, // Updated
-      externalLinkLabels: linkLabels,
-      fieldEditable: true,
-      fieldHeader: fieldHeader,
-      fieldIdx: 2,
-      fieldVisible: true,
-      minColWidth: 50,
-    })
+    params.editMode = true
+    const res = getFieldHeader(params)
     expect(res.cellRendererParams.editMode).toBe(true)
     expect(typeof res.cellClass).toBe('function')
+    expect(typeof res.valueParser).toBe('function')
+  })
 
-    // Test cellClass callback
-    // TODO: Test this in separate tests with different params
-    const cc = (res.cellClass as CellClassFunc)({
-      colDef: { cellRendererParams: { colType: EDIT_COL_TYPE_NAME } },
-      column: {} as Column,
-      value: { editable: true, value: 'x' }
-    } as CellClassParams)
+  test('get edit mode cell class', async () => {
+    params.editMode = true
+    const res = getFieldHeader(params)
+    const cc = (res.cellClass as CellClassFunc)(
+      getCellClassParams(EDIT_COL_TYPE_NAME, true))
     expect(cc).toEqual(['sodar-ss-data-cell', 'text-left'])
+  })
+
+  test('get edit mode cell class with numeric type', async () => {
+    params.editMode = true
+    const res = getFieldHeader(params)
+    const cc = (res.cellClass as CellClassFunc)(
+      getCellClassParams(EDIT_COL_TYPE_NUMERIC, true))
+    expect(cc).toEqual(['sodar-ss-data-cell', 'text-right'])
+  })
+
+  test('get edit mode cell class with unit type', async () => {
+    params.editMode = true
+    const res = getFieldHeader(params)
+    const cc = (res.cellClass as CellClassFunc)(
+      getCellClassParams(EDIT_COL_TYPE_UNIT, true))
+    expect(cc).toEqual(['sodar-ss-data-cell', 'text-right'])
+  })
+
+  test('get edit mode cell class with editable=False', async () => {
+    params.editMode = true
+    const res = getFieldHeader(params)
+    const cc = (res.cellClass as CellClassFunc)(
+      getCellClassParams(EDIT_COL_TYPE_NAME, false))
+    expect(cc).toEqual([
+      'sodar-ss-data-cell', 'text-left', 'bg-light', 'text-muted'])
+  })
+
+  test('get edit mode cell class with editable=False and newInit', async () => {
+    params.editMode = true
+    const res = getFieldHeader(params)
+    const cc = (res.cellClass as CellClassFunc)(
+      getCellClassParams(EDIT_COL_TYPE_NAME, false, true))
+    expect(cc).toEqual([
+      'sodar-ss-data-cell',
+      'text-left',
+      'sodar-ss-data-forbidden',
+      'text-muted'])
   })
 })
